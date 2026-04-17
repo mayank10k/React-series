36be898599c90beb1b030d0ba1d9226b6f57b969
Mayank Mehta <mayankmehta433@gmail.com>
2026-04-17 17:54:17 +0530

    learned how to use module css and tailwind css

---
+++ 05-css/src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './Header.module.css'
+
+const Header = () => {
+  return (
+    <div className={styles.header}>
+      <h1>Mayank Mehta</h1>
+      <button>mai hu button</button>
+      
+    </div>
+  )
+}
+
+export default Header
